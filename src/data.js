a9791a2d70821ab0cf38c5839624059f887611c1
Yash Chauhan <yash848210@gmail.com>
2022-11-23 22:50:28 +0530

    commited

---
+++ src/data.js
@@ -0,0 +1,90 @@
+import img1 from "./assets/img1.JPG";
+import img2 from "./assets/img2.JPG";
+import img3 from "./assets/img3.JPG";
+import img4 from "./assets/img4.JPG";
+import img5 from "./assets/img5.JPG";
+import img6 from "./assets/img6.JPG";
+import img7 from "./assets/img7.JPG";
+import img8 from "./assets/ing8.JPG";
+import img9 from "./assets/img9.JPG";
+import img10 from "./assets/img10.JPG";
+import img11 from "./assets/img11.JPG";
+import img12 from "./assets/img12.JPG";
+import img13 from "./assets/img13.JPG";
+import img14 from "./assets/img14.JPG";
+export const sliderItems = [
+  {
+    id: 1,
+    img: img1,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: img2,
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: img3,
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 4,
+    img: img4,
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 5,
+    img: img5,
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 6,
+    img: img6,
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 7,
+    img: img7,
+  },
+  {
+    id: 8,
+    img: img8,
+  },
+  {
+    id: 9,
+    img: img9,
+  },
+  {
+    id: 10,
+    img: img10,
+  },
+  {
+    id: 11,
+    img: img11,
+  },
+  {
+    id: 12,
+    img: img12,
+  },
+  {
+    id: 7,
+    img: img13,
+  },
+  {
+    id: 8,
+    img: img14,
+  },
+];
